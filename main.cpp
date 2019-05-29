478fbff9e5ab036471cfabde00ad6109a52c80e0
TSKangetsu <x403070567@live.com>
2019-05-29 12:30:58 +0800

    test over fist

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <cpr/cpr.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    cpr::Session session;
-    session.SetUrl("https://www.baidu.com");
-    auto response = session.Get();
-    // auto r = cpr::Get(cpr::Url{"http://www.baidu.com"});
+    // char *headerINFO = "username=";
+    // char *headerINFO2 = "&password=";
+    // char *fucmainINFO = headerINFO + *argv[0] + *headerINFO2 + *argv[1];
+    //http://10.110.2.15/lxportal/getIP.php
+    // std::cout << *fucmainINFO << std::endl;
+    auto response = cpr::Post(cpr::Url{"http://10.110.2.15/lxportal/getIP.php"},
+                              cpr::Body{"username=19924613328&password=A02082635"});
     std::cout << response.text << std::endl;
-    std::cout << response.header["set-cookie"] << std::endl;
-    std::cout << response.status_code << std::endl;
     return 0;
 }
