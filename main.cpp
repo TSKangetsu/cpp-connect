2f152522afb518726fe45ab1a08354cb7bc0155c
TSKangetsu <x403070567@live.com>
2019-06-04 21:38:53 +0800

    opt-use

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
 #include <iostream>
+#include <unistd.h>
 #include <string>
 #include <cpr/cpr.h>
 
-int main(int argc, char *argv[])
+int connect_send(char *usr, char *passwd)
 {
-    std::string username = argv[1];
-    std::string password = argv[2];
+    std::string username = usr;
+    std::string password = passwd;
     auto response = cpr::Post(cpr::Url{"http://10.110.2.15/lxportal/getIP.php"},
                               cpr::Body{"username=" + username + "&password=" + password});
     if (response.text != "0")
     {
         std::cout << "connection failed" << std::endl;
-    }else
+    }
+    else
     {
-        printf("connection succuces");
+        std::cout << "connection success" << std::endl;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int argvs;
+    char *usr;
+    char *passwd;
+    int data_comfirm;
+    while ((argvs = getopt(argc, argv, "u:p:")) != -1)
+    {
+        switch (argvs)
+        {
+        case 'u':
+            usr = optarg;
+            data_comfirm = 1;
+            break;
+        case 'p':
+            passwd = optarg;
+            data_comfirm = 2;
+            break;
+        default:
+            abort();
+            break;
+        }
+    }
+    if (data_comfirm = 2)
+    {
+        connect_send(usr, passwd);
     }
     return 0;
 }
